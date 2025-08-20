45e6b9212930c28b0acc23c2477d7ecc51ff0b98
frontend world <meenakshinain930@gmail.com>
2025-08-20 10:48:15 +0530

    Delete micro project directory

--- micro project/script.js
+++
@@ -1,6 +0,0 @@
-// Contact Form
-document.getElementById("contactForm").addEventListener("submit", function(e){
-  e.preventDefault();
-  document.getElementById("msg").innerText = "✅ Message sent successfully!";
-  this.reset();
-});
